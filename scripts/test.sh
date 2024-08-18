cd02a56b7b7d174c6723f58d11cfac1a2850445c
Walter Dundore <walt@dundore.org>
2024-08-18 14:41:26 -0400

    add test for nfs

--- scripts/test.sh
+++ scripts/test.sh
@@ -27,7 +27,7 @@ vagrant ssh submit -c "sudo scontrol show nodes"
 echo =========================
 echo "Testing Nodes"
 echo =========================
-echo Testing /vagrant status on compute1
-vagrant ssh compute1 -c "sudo ls -la /vagrant"
-echo Testing /vagrant status on compute2
-vagrant ssh compute2 -c "sudo ls -la /vagrant"
+echo Testing /mnt/nfs/scripts status on compute1
+vagrant ssh compute1 -c "sudo ls -la /mnt/nfs/scripts"
+echo Testing /mnt/nfs/scripts status on compute2
+vagrant ssh compute2 -c "sudo ls -la /mnt/nfs/scripts"
